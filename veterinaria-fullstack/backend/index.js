c8ab2d94630420ee06b4a53ee67e4b8d5946eaa6
indiakka <78439130+indiakka@users.noreply.github.com>
2022-03-15 20:02:31 +0100

    cambio puerto

--- veterinaria-fullstack/backend/index.js
+++ veterinaria-fullstack/backend/index.js
@@ -3,6 +3,8 @@ const requestHandler = require("./request-handler");
 const server = http.createServer(requestHandler);
 //el punto despues de http significa que es un objeto
 
-server.listen(5000, () => {
-  console.log("el servidor esta escuchando peticiones");
+server.listen(6000, () => {
+  console.log(
+    "el servidor esta escuchando peticiones en http://localhost:6000/"
+  );
 });
